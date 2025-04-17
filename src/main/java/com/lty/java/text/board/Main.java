44bf86b57740ed3e11c73a425fd373b2220b839b
L2taeyong <kud8918@naver.com>
2025-04-17 22:03:07 +0900

    게시물 작성시 제목, 내용에 대한 입력 유뮤 처리

--- src/main/java/com/lty/java/text/board/Main.java
+++ src/main/java/com/lty/java/text/board/Main.java
@@ -2,36 +2,44 @@ package com.lty.java.text.board;
 
 import java.util.Scanner;
 
-public class Main{
-  public static void main(String[] args){
+public class Main {
+  public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
     System.out.println("== 자바 텍스트 게시판 ==");
-    System.out.println("텍스트 게시판을 시작합니다");
+    System.out.println("텍스트 게시판을 시작합니다.");
 
     while (true) {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals ("/user/article/write")){
-        System.out.println("==게시물 작성==");
+      if (cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 작성 ==");
         System.out.print("제목 : ");
         String subject = sc.nextLine();
+
+        if (subject.trim().isEmpty()) {
+          System.out.println("제목을 입력해주세요.");
+          continue;
+        }
 
         System.out.print("내용 : ");
         String content = sc.nextLine();
 
-        System.out.println("1번 게시물이 등록되었습니다.\n");
-
-    }
-      else if(cmd.equals ("exit")) {
-        System.out.println("텍스트 게시판을 종료합니다");
-        break;
-      }
-      else{
-          System.out.println("잘못 입력 된 명령어입니다.");
+        if (content.trim().isEmpty()) {
+          System.out.println("내용을 입력해주세요.");
+          continue;
         }
 
+        int id = 1;
+
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+      } else if (cmd.equals("exit")) {
+        System.out.println("텍스트 게시판을 종료합니다.");
+        break;
+      } else {
+        System.out.println("잘못 입력 된 명령어입니다.");
+      }
     }
 
     System.out.println("== 자바 텍스트 게시판 종료 ==");
@@ -39,6 +47,3 @@ public class Main{
     sc.close();
   }
 }
-
-
-
