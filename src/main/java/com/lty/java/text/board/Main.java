d4bddb790004b1b68f59b1bfae0f09733d428387
L2taeyong <kud8918@naver.com>
2025-04-17 21:37:15 +0900

    user/article/write 명령어 처리

--- src/main/java/com/lty/java/text/board/Main.java
+++ src/main/java/com/lty/java/text/board/Main.java
@@ -13,12 +13,25 @@ public class Main{
       System.out.print("명령) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals ("exit")){
-        System.out.println("텍스트 게시판을 종료합니다");
-      break;
-    }
+      if(cmd.equals ("/user/article/write")){
+        System.out.println("==게시물 작성==");
+        System.out.print("제목 : ");
+        String subject = sc.nextLine();
 
-      System.out.printf("입력받은 명령어 : %s\n",cmd);
+        System.out.print("내용 : ");
+        String content = sc.nextLine();
+
+        System.out.println("1번 게시물이 등록되었습니다.\n");
+
+    }
+      else if(cmd.equals ("exit")) {
+        System.out.println("텍스트 게시판을 종료합니다");
+        break;
+      }
+      else{
+          System.out.println("잘못 입력 된 명령어입니다.");
+        }
+
     }
 
     System.out.println("== 자바 텍스트 게시판 종료 ==");
