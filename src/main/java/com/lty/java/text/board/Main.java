8c4f4a50c58df4b75c2535080ce8db70ee47bf00
L2taeyong <kud8918@naver.com>
2025-04-17 21:14:20 +0900

    시작 메시지 출력

--- src/main/java/com/lty/java/text/board/Main.java
+++ src/main/java/com/lty/java/text/board/Main.java
@@ -1,7 +1,13 @@
 package com.lty.java.text.board;
 
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
+public class Main{
+  public static void main(String[] args){
+    System.out.println("== 자바 텍스트 게시판 ==");
+    System.out.println("텍스트 게시판을 시작합니다");
+    System.out.println("텍스트 게시판을 종료합니다");
+    System.out.println("== 자바 텍스트 게시판 종료 ==");
   }
 }
+
+
+
